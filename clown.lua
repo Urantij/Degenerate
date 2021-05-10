3fffb84ac25ecdc7a7dfaf3ee4ec53f4868a7d05
Urantij <maximumuranti@gmail.com>
2021-05-10 10:12:15 +0700

    Update clown.lua

--- clown.lua
+++ clown.lua
@@ -1,1 +1,5 @@
-listInv = function(component) for i = 0, 5, 1 do print(i, component.inventory_controller.getInventoryName(i)) end end
+module = {}
+
+module.listInv = function(component) for i = 0, 5, 1 do print(i, component.inventory_controller.getInventoryName(i)) end end
+
+return module
